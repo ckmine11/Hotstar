f3fb9b73c86ecbb955aa41d762532647de9d2bb6
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-05-09 23:24:30 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-cd /var/lib/jenkins/workspace/devsecops-pro/
+cd /var/lib/jenkins/workspace/hotstar-app/
 sudo su - jenkins -s/bin/bash
 #docker logout   master.dns.com
   docker login -u admin --password Harbor12345 master.dns.com
   docker image build -t  $JOB_NAME:v1.$BUILD_ID .
- docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID
- docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/devsecops/$JOB_NAME:latest
- docker image push master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID
- docker  push master.dns.com/devsecops/$JOB_NAME:latest
- docker image rmi $JOB_NAME:v1.$BUILD_ID  master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID   master.dns.com/devsecops/$JOB_NAME:latest 
+ docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/hotstar/$JOB_NAME:v1.$BUILD_ID
+ docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/hotstar/$JOB_NAME:latest
+ docker image push master.dns.com/hotstar/$JOB_NAME:v1.$BUILD_ID
+ docker  push master.dns.com/hotstar/$JOB_NAME:latest
+ docker image rmi $JOB_NAME:v1.$BUILD_ID  master.dns.com/hotstar/$JOB_NAME:v1.$BUILD_ID   master.dns.com/hotstar/$JOB_NAME:latest
