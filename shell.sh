9501c1da2e736cfa5fcbefca7b2accae343a064e
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-08-01 09:28:57 -0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 cd /var/lib/jenkins/workspace/hotstar-app/
 sudo su - jenkins -s/bin/bash
-#docker logout   master.dns.com
-docker login -u admin --password Harbor12345 master.dns.com
+#docker logout  harbor.cluster.com
+docker login -u admin --password root harbor.cluster.com
 docker image build -t  $JOB_NAME:v1.$BUILD_ID .
-docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/hotstar/$JOB_NAME:v1.$BUILD_ID
-docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/hotstar/$JOB_NAME:latest
-docker image push master.dns.com/hotstar/$JOB_NAME:v1.$BUILD_ID
-docker  push master.dns.com/hotstar/$JOB_NAME:latest
-#sudo docker image rmi $JOB_NAME:v1.$BUILD_ID  master.dns.com/hotstar/$JOB_NAME:v1.$BUILD_ID   master.dns.com/hotstar/$JOB_NAME:latest
+docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/hotstar-app/$JOB_NAME:v1.$BUILD_ID
+docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/hotstar-app/$JOB_NAME:latest
+docker image push harbor.cluster.com/hotstar-app/$JOB_NAME:v1.$BUILD_ID
+docker  push harbor.cluster.com/hotstar-app/$JOB_NAME:latest
+#docker image rmi $JOB_NAME:v1.$BUILD_ID  harbor.cluster.com/hotstar-app/$JOB_NAME:v1.$BUILD_ID   harbor.cluster.com/hotstar-app/$JOB_NAME:latest 
